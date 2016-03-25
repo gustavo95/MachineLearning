94e0f2b5b4259255a7abb99942de64bd7b4a5e22
Gustavo Costa Gomes de Melo <gustavo_costa_gomes@hotmail.com>
2016-03-25 13:54:49 -0300

    unused methods removed

--- MachineLearning/src/clustering/Instance.java
+++ MachineLearning/src/clustering/Instance.java
@@ -1,10 +1,6 @@
 package clustering;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
-
-public interface Instance extends Map<Integer, Double>, Iterable<Double>{
+public interface Instance{
 	
 	public double value(int pos);
 	
@@ -13,42 +9,5 @@ public interface Instance extends Map<Integer, Double>, Iterable<Double>{
 	public String toString();
 	
 	public boolean equals(Object obj);
-    
-    public Object classValue();
-
-    public void setClassValue(Object value);
-
-    @Override
-    @Deprecated
-    public int size();
-
-    public Instance minus(Instance min);
-
-    @Override
-    public SortedSet<Integer> keySet();
-
-    public Instance minus(double value);
-
-    public Instance add(Instance max);
-
-    public Instance divide(double value);
-
-    public Instance divide(Instance currentRange);
-
-    public Instance add(double value);
-
-    public Instance multiply(double value);
-
-    public Instance multiply(Instance value);
-
-    public void removeAttribute(int i);
-
-    public Instance sqrt();
-
-    public int getID();
-
-    public Instance copy();
-
-    public void removeAttributes(Set<Integer> indices);
 
 }
